ea235c4cbe2ffe4c35340f0bac3e2df84b0ea657
bvigerzi <48936239+bvigerzi@users.noreply.github.com>
2020-07-26 11:40:10 +1000

    Add REST endpoint to toggle the LED

--- blink.go
+++ blink.go
@@ -1,28 +1,52 @@
 package main
 import (
 	"fmt"
-	"time"
-	"os"
+	"net/http"
+	"encoding/json"
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
+type state struct {
+	On bool
+}
+
 var (
 	pin = rpio.Pin(10)
+	simulate = false
+	on = false
 )
 
-func main() {
-	if err := rpio.Open(); err != nil {
-		fmt.Print(err)
-		os.Exit(1)
-	}
-
-	defer rpio.Close()
-
-	pin.Output()
-
-	for {
+func toggle() {
+	if simulate {
+		fmt.Println("Toggled the pin")
+	} else {
 		pin.Toggle()
-		time.Sleep(time.Second)
+	}
+	on = !on
+}
+
+func init() {
+	fmt.Println("Init the pin")
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		fmt.Println("Failed to init GPIO pin, simulating instead")
+		simulate = true
+	} else {
+		fmt.Println("Finished pin init")
+		pin.Output()
+		pin.Low()
 	}
 }
 
+func toggleHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	toggle()
+	responseWriter.WriteHeader(http.StatusOK)
+	state := state{on}
+	json.NewEncoder(responseWriter).Encode(state)
+}
+
+func main() {
+	http.HandleFunc("/toggle", toggleHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
